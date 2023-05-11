5309056cfc389a8975207563ef80429bdb3815a9
mahmoodi-majid <majid.mahmoodi.pg@gmail.com>
2023-05-11 20:42:20 +0330

    add new style By pressing the positive button, one degree is added to the temperature, and by pressing the negative button, one degree is decreased If the temperature rises above 15, the color of the thermometer turns red, and if it is below 15, the thermometer turns blue

--- src/Components/temp/Temp.js
+++ src/Components/temp/Temp.js
@@ -1,21 +1,52 @@
-import React, { Component } from 'react'
-import "./temp.css"
+import React, { Component } from "react";
+import "./temp.css";
 export default class Temp extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tempDisplaye: 10,
+      tempColor: "cold",
+    };
+  }
+  addTempNumber() {
+    if (this.state.tempDisplaye >= 30) {
+      return false;
+    }
+    this.setState((prevState) => {
+      return { tempDisplaye: prevState.tempDisplaye + 1 };
+    });
+    if (this.state.tempDisplaye >= 15) {
+      this.setState({ tempColor: "hot" });
+    }
+  }
+
+  negativeTempNumber() {
+    if (this.state.tempDisplaye <= 0) {
+      return false;
+    }
+    this.setState((prevState) => {
+      return { tempDisplaye: prevState.tempDisplaye - 1 };
+    });
+    if (this.state.tempDisplaye <= 15) {
+      this.setState({ tempColor: "cold" });
+    }
+  }
+
   render() {
     return (
       <div>
-        <div className='app-container'>
-          <div className='tempPerature-display-container'>
-            <div className='temperature-display cold'>
-                10&#176;C
+        <div className="app-container">
+          <div className="tempPerature-display-container">
+            <div className={`temperature-display ${this.state.tempColor}`}>
+              {this.state.tempDisplaye}
             </div>
           </div>
-          <div className='button-Container'>
-                <button>+</button>
-                <button>_</button>
+          <div className="button-Container">
+            <button onClick={this.addTempNumber.bind(this)}>+</button>
+            <button onClick={this.negativeTempNumber.bind(this)}>_</button>
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
