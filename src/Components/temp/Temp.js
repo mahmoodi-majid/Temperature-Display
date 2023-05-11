f4aa4dddcca8181be4626871864c8115a38a9e0f
mahmoodi-majid <majid.mahmoodi.pg@gmail.com>
2023-05-11 15:19:16 +0430

    start project

---
+++ src/Components/temp/Temp.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+import "./temp.css"
+export default class Temp extends Component {
+  render() {
+    return (
+      <div>
+        <div className='app-container'>
+          <div className='tempPerature-display-container'>
+            <div className='temperature-display cold'>
+                10 C°
+            </div>
+          </div>
+          <div className='button-Container'>
+                <button>+</button>
+                <button>_</button>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
